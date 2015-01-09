b08b17f9f84241903b9c5b5c3a10dfe304394d6e
Quintin Smith <quintin@getweave.com>
2015-01-09 14:13:44 -0700

    Initial Commit

---
+++ CustomSegues/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomSegues
+//
+//  Created by Quintin Smith on 1/9/15.
+//  Copyright (c) 2015 Quintin Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
